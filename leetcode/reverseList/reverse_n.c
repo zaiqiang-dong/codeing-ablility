8858e159dc996fc1e2c385c37d3d53f3f9d18cea
Zaiqiang Dong <zqdong@nreal.ai>
2021-04-08 19:45:35 +0800

    update

--- leetcode/reverseList/reverse_n.c
+++ leetcode/reverseList/reverse_n.c
@@ -11,13 +11,13 @@
 
 struct ListNode *last_n = NULL;
 
-struct ListNode * reverseN(struct ListNode *head, int n)
+struct ListNode * reversen(struct ListNode *head, int n)
 {
 	if (n == 1) {
 		last_n = head->next;
 	}
 
-	struct ListNode *last = reverseN(head->next, n-1);
+	struct ListNode *last = reversen(head->next, n-1);
 
 	head->next->next = head;
 	head->next = last_n;
