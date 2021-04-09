c33706db2a7e8f6b2a32489f670e001ca4ccbc9d
Zaiqiang Dong <zqdong@nreal.ai>
2021-04-09 09:16:17 +0800

    update

--- leetcode/reverseList/reverse_kgroup.c
+++ leetcode/reverseList/reverse_kgroup.c
@@ -7,19 +7,15 @@
  * };
  */
 
-// 反转整个链表
+ struct ListNode {
+     int val;
+     struct ListNode *next;
+ };
+
+// 递代反转整个链表
 
 struct ListNode * reverse(struct ListNode *head)
 {
-	if (head->next == NULL) {
-		return head;
-	}
-
-	struct ListNode *last = reverse(head->next);
-
-	head->next->next = head;
-	head->next = NULL;
-
-	return last;
+	struct ListNode *pre,*cur,*nxt;
 
 }
